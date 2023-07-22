3b00df74bade74f893d2e829cd6564b331cb31a2
vanpl-matthew <123037645+vanpl-matthew@users.noreply.github.com>
2023-07-22 13:41:48 +0900

    프로젝트 생성

---
+++ Component/Component/ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentApp.swift
+//  Component
+//
+//  Created by matthew on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
